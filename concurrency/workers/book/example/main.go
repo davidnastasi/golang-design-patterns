6715a5fea603006721404dca0a79ea9c2fe7d3a3
David Alejandro Nastasi <david.nastasi@brubank.com>
2021-05-24 23:01:38 -0300

    add worker example

--- concurrency/workers/book/example/main.go
+++ concurrency/workers/book/example/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"go-design-patterns/concurrency/workers"
+	"go-design-patterns/concurrency/workers/book"
 	"sync"
 )
 
 func main() {
 	bufferSize := 100
-	var dispatcher  = workers.NewDispatcher(bufferSize)
+	var dispatcher  = book.NewDispatcher(bufferSize)
 
 	wk := 3
 	for i := 0; i < wk ; i++ {
-		var w workers.WorkerLauncher = &workers.PrefixSuffixWorker{
+		var w book.WorkerLauncher = &book.PrefixSuffixWorker{
 			PrefixS: fmt.Sprintf("WorkerID: %d ->",i),
 			SuffixS: "World",
 			ID: i,
@@ -25,7 +25,7 @@ func main() {
 	wg.Add(request)
 
 	for i:=0; i < request ; i++ {
-		req := workers.NewStringRequest("Hello ", i , &wg )
+		req := book.NewStringRequest("Hello ", i , &wg )
 		dispatcher.MakeRequest(req)
 	}
 	dispatcher.Stop()
--- concurrency/workers/book/worker.go
+++ concurrency/workers/book/worker.go
@@ -1,4 +1,4 @@
-package workers
+package book
 
 import (
 	"fmt"
